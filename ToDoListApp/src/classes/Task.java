75f363794c75b614fd77ff6243362f8c8e0e1664
maelaiza <elaiza.ilagan@awsys-i.com>
2024-10-15 14:40:15 +0800

    Enhance deadline feature: change to LocalDate,add validation, update toString

--- ToDoListApp/src/classes/Task.java
+++ ToDoListApp/src/classes/Task.java
@@ -1,16 +1,22 @@
 package classes;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+
 public class Task {
     private static int idCounter = 1;
     private int id;
     private String description;
-    private String deadline;
+    private LocalDate deadline;
+    //private String deadline;
     private boolean isCompleted;
 
     public Task(String description, String deadline) {
         this.id = idCounter++;
         this.description = description;
-        this.deadline = deadline;
+        setDeadline(deadline);
+        //this.deadline = deadline;
         this.isCompleted = false;
     }
 
@@ -22,7 +28,7 @@ public class Task {
         return description;
     }
 
-    public String getDeadline() {
+    public LocalDate getDeadline() {
         return deadline;
     }
 
@@ -32,6 +38,16 @@ public class Task {
 
     public void markAsCompleted() {
         this.isCompleted = true;
+    }
+    
+    public void setDeadline(String deadline) {
+    	try {
+    		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+    		this.deadline = LocalDate.parse(deadline,formatter);
+    	}	catch(DateTimeParseException e) {
+    		System.out.println("Invalid date format. Please use 'yyyy-MM-dd'.");
+    		this.deadline = null;
+    	}
     }
 
     @Override
